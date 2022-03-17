d8769e323bad8c74bd834ce03803a2a689c046c1
Asi Meir <asi.meir@gmail.com>
2022-03-17 17:31:56 +0200

    working version

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-  const allbanianNouns = require ('./scrapper.js');   
+  const {getAllAlbanianNouns} = require ('./scrapper.js');   
 
 async function start() {
   //scrape the wiki page for the albanian nouns
   const allbanianNouns = await getAllAlbanianNouns();
-
+  console.log(allbanianNouns.length);
   //write a test that checks if the nouns are in the array
 
   // assert "abakë" in all_albanian_nouns
--- scrapper.js
+++ scrapper.js
@@ -1,15 +1,35 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const axios = require('axios');
 
-const URL = 'https://en.wiktionary.org/wiki/Category:Albanian_nouns';
+async function getAllAlbanianNouns() {
+  let firstWord = 'aba';
+  let totalNouns = [];
+  https://en.wiktionary.org/w/index.php?title=Category:Albanian_nouns&pagefrom=${firstWord}#mw-pages
+  try {
+    while (true) {
+      const URL_TEMPLATE = `https://en.wiktionary.org/w/index.php?title=Category:Albanian_nouns&pagefrom=${firstWord}#mw-pages`;
+      const encodedUrl = encodeURI(URL_TEMPLATE)
+      const { data } = await axios.get(encodedUrl);
 
-export async function getAllAlbanianNouns(){
-  debugger;
-  const response = await fetch(URL);
-  const $ = cheerio.load('<ul id="fruits">...</ul>');
+      const $ = cheerio.load(data);
+      const selector = '#mw-pages > div > div > div > ul > li > a ';
+      const anchors = $(selector);
+      const nouns = anchors.map((i, el) => $(el).text()).get();
+      console.log(nouns)
+      if (nouns.length === 1) {
+        return;
+      }
 
-  $.html();
-
-  console.log(response);
-  return [];
+      const lastWord = [...nouns].pop();
+      firstWord = lastWord;
+      console.log(lastWord);
+      totalNouns.push(...nouns);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  return totalNouns;
 }
+
+module.exports = { getAllAlbanianNouns };
